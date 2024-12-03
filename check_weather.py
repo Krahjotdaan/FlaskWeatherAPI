14e93a5cfe422cc07e1ddaabdade445742071a51
Artem Ostapenko <a.ostapenko@edu.centraluniversity.ru>
2024-12-03 11:48:41 +0300

    Feat: added result string creation

--- check_weather.py
+++ check_weather.py
@@ -1,5 +1,3 @@
-import json
-
 
 def get_main_params(data: dict):
     main_params = {}
@@ -14,6 +12,27 @@ def get_main_params(data: dict):
     main_params["pressure"] = data["Pressure"]["Metric"]["Value"]
 
     return main_params
+
+
+def get_result_str(main_params: dict):
+    result_str = ""
+
+    result_str += f"Температура: {main_params["temp"]}C \n"
+    result_str += f"Ощущается как: {main_params["feel_temp"]}C \n"
+    result_str += f"Погода: {main_params["weather_text"]} \n"
+    result_str += f"Осадки: {main_params["precipitation"]} \n"
+    result_str += f"Относительная влажность: {main_params["humidity"]}% \n"
+    result_str += f"Скорость ветра: {main_params["wind_speed"]} м/c \n"
+    result_str += f"Направление ветра: {main_params["direction"]} \n"
+    result_str += f"Давление: {main_params["pressure"]} гПа \n"
+
+    is_bad_weather = check_bad_weather(main_params)
+    if is_bad_weather:
+        result_str += f"Погода неблагоприятная \n"
+    else:
+        result_str += f"Погода хорошая \n\n"
+
+    return result_str
 
 
 def check_bad_weather(data):
